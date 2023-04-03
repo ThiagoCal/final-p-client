87ed0d4a2d1083ef261f385bf7c33d632a75c719
ThiagoCal <thiagotorrescalazans@gmail.com>
2023-04-03 18:07:21 +0300

    front-redo

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Component/Home'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Component/Home.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import 'mapbox-gl/dist/mapbox-gl.css';
+// import mapboxgl from '!mapbox-gl';
+import axios from 'axios';
+
+export const Home = (props) => {
+  const [parties, setParties] = useState('')
+
+
+  useEffect(()=>{
+    const getParties = async() =>{
+      let response = await axios.get('/parties')
+      console.log(response.data)
+      setParties(response.data)
+    }
+    getParties()
+    console.log('parties', parties)
+  },[])
+
+  return (
+    <>
+      <div>Home</div>
+    </>
+  )
+}
+
+
+export default Home
