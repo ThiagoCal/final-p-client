98bfa7d0e91c21e64c3f1e3a5ecf456a1b788694
ThiagoCal <thiagotorrescalazans@gmail.com>
2023-05-11 14:53:44 +0300

    worked on navbar to lead to about us

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -4,9 +4,27 @@ export default function Footer() {
   return (
     <footer class="footer">
       <div class="footer-links">
-        <a href="https://github.com/ThiagoCal">My Github</a>
-        <a href="#">Portfolio</a>
-        <a href="https://www.linkedin.com/in/thiagorossitercal/">Linkedin</a>
+        <a
+          href="https://github.com/ThiagoCal"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My Github
+        </a>
+        <a
+          href="https://thiago-kimel-portfolio.vercel.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Portfolio
+        </a>
+        <a
+          href="https://www.linkedin.com/in/thiagorossitercal/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Linkedin
+        </a>
       </div>
       <p>&copy; 2023 My Messiba Pal. All rights reserved.</p>
     </footer>
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -159,7 +159,7 @@ const Navbar2 = (props) => {
                 key={"about"}
               >
                 <Link
-                  to="/"
+                  to="/aboutus"
                   className=" text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-zinc-400"
                   data-te-nav-link-ref
                 >
